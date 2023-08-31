ebc7e29229f94db4526c7e507178bf2b0a24860a
lorypelli <87276663+lorypelli@users.noreply.github.com>
2023-08-31 22:41:57 +0200

    Replace also bashrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Script is starting copying files"
 
+rm -rf $HOME/.bashrc
 rm -rf $HOME/.config/fish/
 rm -rf $HOME/.config/neofetch/
 rm -rf $HOME/.config/rofi/
@@ -8,6 +9,7 @@ rm -rf $HOME/.config/tmux/
 
 git clone https://github.com/lorypelli/rice $HOME/rice-temp
 
+cp -r $HOME/rice/bash/.bashrc $HOME/.bashrc
 cp -r $HOME/rice-temp/fish/ $HOME/.config/
 cp -r $HOME/rice-temp/neofetch/ $HOME/.config/
 cp -r $HOME/rice-temp/rofi/ $HOME/.config/
