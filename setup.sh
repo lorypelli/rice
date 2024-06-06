0c8c25f161d1e11fe6a12767015901ed79178708
LoryPelli <lorypelli@proton.me>
2024-06-06 16:08:24 +0200

    Install packages

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.config/fish/
-rm -rf $HOME/.config/neofetch/
 rm -rf $HOME/.config/qutebrowser/
 rm -rf $HOME/.config/rofi/
 rm -rf $HOME/.config/tmux/
@@ -11,10 +10,11 @@ git clone https://github.com/lorypelli/rice $HOME/rice-temp/
 git clone https://github.com/dracula/qutebrowser-dracula-theme $HOME/rice-temp/qutebrowser/dracula/
 git clone https://github.com/tmux-plugins/tpm $HOME/rice-temp/tmux/plugins/
 
+sudo pacman -S fish rofi tmux
+
 cp -r $HOME/rice-temp/bash/.bashrc $HOME/.bashrc
 cp -r $HOME/rice-temp/utils/ $HOME/
 cp -r $HOME/rice-temp/fish/ $HOME/.config/
-cp -r $HOME/rice-temp/neofetch/ $HOME/.config/
 cp -r $HOME/rice-temp/qutebrowser/ $HOME/.config/
 cp -r $HOME/rice-temp/rofi/ $HOME/.config/
 cp -r $HOME/rice-temp/tmux/ $HOME/.config/
