d01002cfc3459b42a6629ed740337f7010d3f1e8
lorypelli <lorypelli@proton.me>
2024-06-07 20:17:19 +0200

    Install alacritty and change i3 config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.config/fish/
+rm -rf $HOME/.config/i3/
 rm -rf $HOME/.config/qutebrowser/
 rm -rf $HOME/.config/rofi/
 rm -rf $HOME/.config/tmux/
@@ -10,13 +11,14 @@ git clone https://github.com/lorypelli/rice $HOME/rice-temp/
 git clone https://github.com/dracula/qutebrowser-dracula-theme $HOME/rice-temp/qutebrowser/dracula/
 git clone https://github.com/tmux-plugins/tpm $HOME/rice-temp/tmux/plugins/
 
-sudo pacman -S fish rofi tmux
+sudo pacman -S alacritty fish rofi tmux
 
 cp -r $HOME/rice-temp/bash/.bashrc $HOME/.bashrc
-cp -r $HOME/rice-temp/utils/ $HOME/
 cp -r $HOME/rice-temp/fish/ $HOME/.config/
+cp -r $HOME/rice-temp/i3 $HOME/.config/
 cp -r $HOME/rice-temp/qutebrowser/ $HOME/.config/
 cp -r $HOME/rice-temp/rofi/ $HOME/.config/
 cp -r $HOME/rice-temp/tmux/ $HOME/.config/
+cp -r $HOME/rice-temp/utils $HOME/
 
 rm -rf $HOME/rice-temp/
