bcd6468408bbd251f5ab61d693eee88ed7cfe61e
LoryPelli <lorypelli@proton.me>
2024-08-21 11:03:02 +0200

    Basic nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,78 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.termguicolors = true
+vim.opt.relativenumber = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"stevearc/oil.nvim",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			local mason = require("mason")
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			local conform = require("conform")
+
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					svelte = { "prettier" },
+					astro = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					lua = { "stylua" },
+					python = { "isort", "black" },
+				},
+				format_after_save = {
+					lsp_fallback = true,
+					async = true,
+				},
+			})
+		end,
+	},
+})
+
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+	},
+})
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.config/fish/
 rm -rf $HOME/.config/i3/
+rm -rf $HOME/.config/nvim/
 rm -rf $HOME/.config/qutebrowser/
 rm -rf $HOME/.config/rofi/
 rm -rf $HOME/.config/tmux/
@@ -11,11 +12,12 @@ git clone https://github.com/lorypelli/rice $HOME/rice-temp/
 git clone https://github.com/dracula/qutebrowser-dracula-theme $HOME/rice-temp/qutebrowser/dracula/
 git clone https://github.com/tmux-plugins/tpm $HOME/rice-temp/tmux/plugins/
 
-sudo pacman -S alacritty fish rofi tmux
+sudo pacman -S alacritty fish neovim rofi tmux
 
 cp -r $HOME/rice-temp/bash/.bashrc $HOME/.bashrc
 cp -r $HOME/rice-temp/fish/ $HOME/.config/
-cp -r $HOME/rice-temp/i3 $HOME/.config/
+cp -r $HOME/rice-temp/i3/ $HOME/.config/
+cp -r $HOME/rice-temp/nvim/ $HOME/.config/
 cp -r $HOME/rice-temp/qutebrowser/ $HOME/.config/
 cp -r $HOME/rice-temp/rofi/ $HOME/.config/
 cp -r $HOME/rice-temp/tmux/ $HOME/.config/
