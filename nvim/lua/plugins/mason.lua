cc4cf0fab71468ce4648c8913ed63b37ae5c10d2
LoryPelli <lorypelli@proton.me>
2024-09-05 14:40:42 +0200

    Separate lsp

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+	"williamboman/mason.nvim",
+	opts = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+	end,
+}
