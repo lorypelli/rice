25e57bec2f8c275a73dc2230982cb0a800a602a2
LoryPelli <lorypelli@proton.me>
2024-09-11 14:44:22 +0200

    Fix lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,27 +1,24 @@
 return {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
     lazy = true,
     event = {
         "BufReadPost"
     },
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         "lukas-reineke/lsp-format.nvim",
         "hrsh7th/cmp-nvim-lsp"
     },
-    config = function()
-        local mason_lsp = require("mason-lspconfig")
-        mason_lsp.setup({
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                        on_attach = function(client, bufnr)
-                            require("lsp-format").on_attach(client, bufnr)
-                        end
-                    })
-                end
-            }
-        })
-    end
+    opts = {
+        handlers = {
+            function(server_name)
+                require("lspconfig")[server_name].setup({
+                    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                    on_attach = function(client, bufnr)
+                        require("lsp-format").on_attach(client, bufnr)
+                    end
+                })
+            end
+        }
+    }
 }
