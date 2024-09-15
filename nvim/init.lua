56c43e9f11e244884303b53904afa53f42ccd78e
LoryPelli <lorypelli@proton.me>
2024-09-15 16:38:46 +0200

    Separate lazyrepo variable

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,12 +4,13 @@ vim.g.maplocalleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
         "--branch=stable",
+        lazyrepo,
         lazypath,
     })
 end
