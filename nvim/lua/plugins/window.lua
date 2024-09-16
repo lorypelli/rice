e201a616749029deae09b15c46df86822ab3f214
LoryPelli <lorypelli@proton.me>
2024-09-16 19:29:04 +0200

    Highlight current window

---
+++ nvim/lua/plugins/window.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-zh/colorful-winsep.nvim",
+    lazy = true,
+    event = {
+        "WinNew"
+    },
+    opts = {
+        hi = {
+            fg = "#87CEEB"
+        }
+    }
+}
