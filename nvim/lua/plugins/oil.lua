a397547ef0773cd84bca984a3b6e0b6406c35e40
LoryPelli <lorypelli@proton.me>
2024-12-30 11:40:06 +0100

    don't load oil if not a directory

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,22 @@
 return {
     "stevearc/oil.nvim",
     lazy = true,
-    event = "VimEnter",
+    cmd = "Oil",
     keys = {
         {
             "<leader>e",
             "<cmd>Oil<CR>"
         }
     },
+    init = function()
+        if vim.fn.argc() == 1 then
+            local arg = vim.fn.argv(0)
+            local stat = (vim.uv or vim.loop).fs_stat(arg)
+            if stat and stat.type == "directory" then
+                vim.cmd("Oil " .. vim.fn.fnameescape(arg))
+            end
+        end
+    end,
     opts = {
         skip_confirm_for_simple_edits = true,
         view_options = {
